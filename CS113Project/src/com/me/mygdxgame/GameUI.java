fa733cc7d8d36faf265b2487418249bdf6f2e465
MasterBirdy <mtito@uci.edu>
2012-11-28 17:02:03 -0800

    Added icons.

--- CS113Project/src/com/me/mygdxgame/GameUI.java
+++ CS113Project/src/com/me/mygdxgame/GameUI.java
@@ -10,12 +10,18 @@ public class GameUI
 {
 	static SpriteBatch batch;
 	private TextureRegion sideUI;
+	private TextureRegion swordIcon;
+	private TextureRegion bowIcon;
+	private TextureRegion serfIcon;
 	BitmapFont font;
 	static EverythingHolder everything;
 	
 	public GameUI()
 	{
 		sideUI = new TextureRegion(new Texture(Gdx.files.internal("images/sideui.png")), 200, 480);
+		swordIcon = new TextureRegion(new Texture(Gdx.files.internal("images/swordicon.png")), 48, 48);
+		bowIcon = new TextureRegion(new Texture(Gdx.files.internal("images/bowicon.png")), 48, 48);
+		serfIcon = new TextureRegion(new Texture(Gdx.files.internal("images/serficon.png")), 48, 48);
 		font = new BitmapFont();
 	}
 	
@@ -28,6 +34,9 @@ public class GameUI
 	public void render()
 	{
 		batch.draw(sideUI, Gdx.graphics.getWidth() / 2 - sideUI.getRegionWidth(), Gdx.graphics.getHeight() / 2 - sideUI.getRegionHeight());
+		batch.draw(swordIcon, Gdx.graphics.getWidth() / 2 - sideUI.getRegionWidth() + 10 + 5, Gdx.graphics.getHeight() / 2 - swordIcon.getRegionHeight() * 2 - 10);
+		batch.draw(bowIcon, Gdx.graphics.getWidth() / 2 - sideUI.getRegionWidth() + 10 * 2 + swordIcon.getRegionWidth() + 5, Gdx.graphics.getHeight() / 2 - swordIcon.getRegionHeight() * 2 - 10);
+		batch.draw(serfIcon, Gdx.graphics.getWidth() / 2 - sideUI.getRegionWidth() + 10 * 3 + swordIcon.getRegionWidth() * 2 + 5, Gdx.graphics.getHeight() / 2 - swordIcon.getRegionHeight() * 2 - 10);
 		font.draw(batch, "Total Units: " + (everything.team(1).size() + everything.team(2).size()), Gdx.graphics.getWidth() / 2 - 150, Gdx.graphics.getHeight() / 2 - 20);
 	}
 	
